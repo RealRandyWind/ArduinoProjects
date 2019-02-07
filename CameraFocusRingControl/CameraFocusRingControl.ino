03a329f0943f0317f37ab8bd09ae545d636df4a6
Randy Wind <randywind@live.nl>
2019-02-07 03:08:01 +0100

    Camera Focus Ring Conroll project add.

---
+++ CameraFocusRingControl/CameraFocusRingControl.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		CameraFocusRingControl.ino
+ Created:	2/7/2019 3:07:04 AM
+ Author:	Gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
