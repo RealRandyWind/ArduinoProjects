dd0976ad000477876dcda9d1f9092429e5c2769b
Randy Wind <randywind@live.nl>
2019-02-07 23:33:33 +0100

    CameraFocusRingControl needs testing.

--- CameraFocusRingControl/CameraFocusRingControl.ino
+++ CameraFocusRingControl/CameraFocusRingControl.ino
@@ -1,15 +1,46 @@
 /*
  Name:		CameraFocusRingControl.ino
  Created:	2/7/2019 3:07:04 AM
- Author:	Gebruiker
+ Author:	RealRandyWind (Randy Wind)
+ Email:		wind.randy@gmail.com
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+#define P1 9
+#define P2 10
+#define P3 11
+#define P4 12
+#define JX A0
 
+#define JREF 511
+#define JNORM 32
+#define JMIN 1
+
+#define P1M 1
+#define P2M 2
+#define P3M 4
+#define P4M 8
+#define PALLM 15
+
+#define ONE 1
+#define CSHIFT(DIR, STATE) ((DIR) ? ((STATE) << ONE | (STATE) >> PALLM) : ((STATE) >> ONE | (STATE) << PALLM)) & PALLM
+#define JNORMALIZE(VALUE)  JNORM - ((VALUE) % (JREF + 1)) * (JNORM / JREF) + JMIN
+
+uint8_t state = ONE;
+
+void setup() {
+	pinMode(P1, OUTPUT);
+	pinMode(P2, OUTPUT);
+	pinMode(P3, OUTPUT);
+	pinMode(P4, OUTPUT);
+	pinMode(JX, INPUT);
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	uint8_t vX = analogRead(JX);
+	digitalWrite(P1, P1M & state);
+	digitalWrite(P2, P2M & state);
+	digitalWrite(P3, P3M & state);
+	digitalWrite(P4, P4M & state);
+	state = CSHIFT(JREF < vX, state);
+	delay(JNORMALIZE(vX));
 }
